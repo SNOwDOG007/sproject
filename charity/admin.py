690100908e5a65408f07d53642317c49da5f8fa6
Nakarmi <bpin.nakarmi77@gmail.com>
2020-09-06 01:40:46 +0545

    gallery-state-volunteers

--- charity/admin.py
+++ charity/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from charity.models import Blog
+from charity.models import Blog, Gallery, Volunteer, State
 
 # Register your models here.
 # @admin.register(Blog)
 class CharityAdmin(admin.ModelAdmin):
+    #BLOG 
     list_display = ("title", "author", "created_at")
     prepopulated_fields = {"slug": ("title",)}
     # readonly_fields = ("author",)
 
+    #GALLERY
+    list_display = ("image", "title")
+    prepopulated_fields = {"slug": ("title",)}
+
+    #VOLUNTEER
+    list_display = ("position", "name", "address", "email", "contact")
+    prepopulated_fields = {"slug": ("title",)}
+
+    #STATE
+    list_display = ("donation", "volunteers", "rescued")
+    prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(Blog)
+admin.site.register(Gallery)
+admin.site.register(Volunteer)
+admin.site.register(State)
--- charity/models.py
+++ charity/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.core.validators import RegexValidator
 
 # Create your models here.
 # DJANGO-ORM (Object relational mapping)
@@ -16,4 +17,29 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     cover_image = models.ImageField(upload_to="blog", null=True)
+
+    def get_absolute_url(self):
+        return reverse("single_blog", kwargs={"pk": self.pk, "slug": self.slug})
+
+#GALLERY
+class Gallery(models.Model):
+    image = models.ImageField(upload_to="gallery", null=True)
+    title = models.CharField(max_length=255)
+
+#VOLUNTEER
+class Volunteer(models.Model):
+    position = models.CharField(max_length=75)
+    name = models.CharField(max_length=255)    
+    address = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone_regex = RegexValidator(regex=r'^\d{10}$', message="Phone number must be entered in the format: '98********'. Up to 10 digits allowed.")
+    contact = models.CharField(validators=[phone_regex], max_length=10, null=True)
+    joined_from = models.DateTimeField(auto_now=True)
+    # slug = models.SlugField(max_length=255, null=True)
+
+#STATE
+class State(models.Model):
+    donation = models.IntegerField()
+    volunteers = models.IntegerField()
+    rescued = models.IntegerField()
 
